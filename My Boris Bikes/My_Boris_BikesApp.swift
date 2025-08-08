64ee6f4a8cc1683fe905f7c953a88c1b79dc67e6
Mike Wagstaff <mike.wagstaff@gmail.com>
2025-08-08 18:57:24 +0100

    Initial Commit

---
+++ My Boris Bikes/My_Boris_BikesApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_Boris_BikesApp.swift
+//  My Boris Bikes
+//
+//  Created by Mike Wagstaff on 08/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct My_Boris_BikesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ My Boris BikesTests/My_Boris_BikesTests.swift
@@ -0,0 +1,17 @@
+//
+//  My_Boris_BikesTests.swift
+//  My Boris BikesTests
+//
+//  Created by Mike Wagstaff on 08/08/2025.
+//
+
+import Testing
+@testable import My_Boris_Bikes
+
+struct My_Boris_BikesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
